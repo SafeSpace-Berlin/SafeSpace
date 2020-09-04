31146ff29fb7193a2f7c8f65e72119eddb50f241
Filip Twardzik <ftwardzik@kamioni.de>
2020-09-04 15:53:26 +0200

    Implement first draft of Home scrollable view

--- safespace-frontend/src/pages/home/Home.js
+++ safespace-frontend/src/pages/home/Home.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
 import "./Home.scss";
+import { title, content } from './data'
 
 export default class Home extends Component {
     render() {
         return (
-            <div id="home">
-                <h1>hallo tu Home</h1>
+            <div className="home">
+                <div className="home__section home__section--welcome">
+                </div>
+                <div className="home__section home__section--description">
+                  <div className="content">
+                    <div className="content__title">
+                      {title}
+                    </div>
+                    <div className="content__text">
+                      {content}
+                    </div>
+                  </div>
+                </div>
+                <div className="home__section home__section--get-involved">
+                  <div className="content">
+                    <div className="content__title">
+                      Get involved
+                    </div>
+                  </div>
+                </div>
             </div>
         )
     }
