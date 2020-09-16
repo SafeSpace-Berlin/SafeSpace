35bc6a712a39646e48079ef154e26261ff6be51f
Adrianna Juchacz <adrianna.juchacz@gmail.com>
2020-09-16 12:33:06 +0200

    added toJSON method for Room schema (id convert)

--- safespace-backend/src/models/Room.js
+++ safespace-backend/src/models/Room.js
@@ -1,15 +1,23 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
-var RoomSchema = new mongoose.Schema({
-  roomId: String,
-  availableFrom: Date,
-  availableTo: Date,
-  price: String,
-  country: String,
-  city: String,
-  district: String,
-  photo: String,
-  contact: String
-}, {timestamps: true});
+var RoomSchema = new mongoose.Schema(
+  {
+    availableFrom: Date,
+    availableTo: Date,
+    price: String,
+    country: String,
+    city: String,
+    district: String,
+    photo: String,
+    contact: String,
+  },
+  { timestamps: true }
+);
 
-mongoose.model('Room', RoomSchema);
+RoomSchema.methods.toJSON = function () {
+  const { ...object } = this.toObject();
+  object.id = this._id;
+  return object;
+};
+
+mongoose.model("Room", RoomSchema);
