abae45085a30ecf156a3254e554bcf688fd819a6
Adrianna Juchacz <adrianna.juchacz@gmail.com>
2020-09-16 11:56:41 +0200

    /rooms list if all rooms

---
+++ safespace-frontend/src/config.js
@@ -0,0 +1,5 @@
+var config = {};
+
+config.url = process.env.BACKEND_URL || 'http://localhost:5000/';
+
+module.exports = config;
--- safespace-frontend/src/pages/rooms/Rooms.js
+++ safespace-frontend/src/pages/rooms/Rooms.js
@@ -1,12 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import "./Rooms.scss";
 
+var config = require("../../config");
+
 export default class Rooms extends Component {
-    render() {
-        return (
-            <div id="rooms">
-                <h1>hallo, its Rooms</h1>
+  constructor(props) {
+    super(props);
+    this.state = {
+      rooms: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(config.url + "rooms", {
+      method: "get",
+    })
+      .then((results) => {
+        return results.json();
+      })
+      .then((data) => {
+        let rooms = data.rooms;
+        this.setState({ rooms: this.state.rooms.concat(rooms) });
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }
+  render() {
+    return (
+      <div id="rooms">
+        {this.state.rooms &&
+          this.state.rooms.map((r) => (
+            <div className="field" key={r._id}>
+              <img src={r.photo} alt=""></img>
+              <h2>availableFrom: {r.availableFrom}</h2>
+              <h2>availableTo: {r.availableTo}</h2>
+              <h2>city: {r.city}</h2>
+              <h2>country: {r.country}</h2>
+              <h2>district: {r.district}</h2>
+              <h2>price: {r.price}</h2>
             </div>
-        )
-    }
+          ))}
+      </div>
+    );
+  }
 }
