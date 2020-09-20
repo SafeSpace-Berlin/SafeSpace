356798cae7db8eb2c87c822e6aec9be011ce6a4f
Jacky Lai <jacky.lai@filmuniversitaet.de>
2020-09-20 13:32:07 +0200

    Add mission frontend page

--- safespace-frontend/src/pages/mission/Mission.js
+++ safespace-frontend/src/pages/mission/Mission.js
@@ -5,8 +5,26 @@ export default class Mission extends Component {
     render() {
         return (
             <div id="mission">
-                <h1>halo tu Mission</h1>
+                <div class="row">
+                    <div class="column left">
+                            <h1>Our Mission</h1>
+                    </div>
+                    <div class="column right">
+                            <h3>What is SafeSpace?</h3>
+                            <p>SafeSpace is a platform where non-heteronormative, non-binary, transgender and queer people who found themselves in jeopardy due to their sexual identity have the chance to find accommodation in Berlin. Bearing in mind how difficult this process is, LGBTQ+ (friendly) hosts can make this obstacle easier to overcome by offering their safe spaces (rooms/entire apartments) to those who need them most.</p>
+                            
+                            <h3>How can I use SafeSpace?</h3>
+                            <p>SafeSpace is a place where potential tenants can communicate with hosts free of charge. As a SafeSpaceTeam we want to give you this tool to help or seek help, but we are not involved in any administrative processes related to the rental. </p>
+                            
+                            <h3>Why has Safe Space been created?</h3>
+                            <p>SafeSpace is directly inspired by the spread of homophobia in Poland, particularly by enacting so-called "LGBT-free zones" in numerous regions, mainly in the south-eastern part of the country. At the moment, they are making up a third of Polish territory. The resolution aims to fight against an alleged "LGBT-ideology" and ban any LGBTQ+ events.</p>
+                            
+                            <p>Although this discriminatory law doesn't have any legal force, it has become a symbol of the homophobia, LGBTQ+ community in Poland has been struggling with for years. In recent times, LGBTQ+ rights have grown to be one of the leading topics of public debate in Poland, taken up also by right-wing populists, many of whom have identified LGBTQ+ movement as a "threat to the traditional family model". 
+                                <br></br><br></br>Homophobia in Poland is openly and in most cases with impunity expressed by the hierarchs of the Catholic Church, politicians (including President of Poland Andrzej Duda whose campaign before Presidential Election in July 2020 was based on anti-LGBT rhetoric) and non-governmental fundamentalist organisations such as Fundacja Prawo do Życia (Foundation Right to Life) that supports i. a. the project "Stop Pedofilii" (Stop Paedophilia) aiming to penalise any form of sexual education and recognising homosexuality as a source of paedophilia.</p>              
+                    </div>
+                </div>      
             </div>
+ 
         )
     }
 }
