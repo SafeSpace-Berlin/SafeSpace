63052c4c93434b65b948ec7220d54abf522b0afe
Filip Twardzik <twardzikf@gmail.com>
2020-09-28 14:43:57 +0200

    Implement the form to add a room offer

---
+++ safespace-frontend/src/pages/offerRoom/data.js
@@ -0,0 +1,15 @@
+export const data = {
+    description: {
+        title: 'Offer a room',
+        text: 'LOrem ipsum dolor sit amet. or sit amet. LOrem ip or sit amet. LOrem ip or sit amet. LOrem ipLOrem ipsum dolor sit amet. LOrem ipsum dolor sit amet. LOrem ipsum dolor sit amet. LOrem ipsum dolor sit amet.'
+    },
+    formLabels: {
+        city: 'city',
+        rent: 'rent',
+        district: 'district',
+        description: 'description',
+        email: 'email',
+        submit: 'submit',
+        uploadPhoto: 'upload photo',
+    },
+}
